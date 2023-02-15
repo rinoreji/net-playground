e2f027c6ab4ad85cdc3cdbf032659cc54a54eaa4
Rino Reji Cheriyan <mailrino@gmail.com>
2023-02-15 08:38:50 +0000

    base impl

---
+++ handlebarsDotNet/Program.cs
@@ -0,0 +1,36 @@
+﻿using HandlebarsDotNet;
+
+// Handlebars.Configuration.Compatibility.RelaxedHelperNaming=false;
+
+Handlebars.RegisterHelper("#sph", (writer, context, arguments) =>
+{
+  writer.Write("'h- w'");
+});
+
+string source =
+@"<div class=""entry"">
+  <h1>{{#sph 'title' 'xyz'}}</h1>
+  <div class=""body"">
+    {{body}}
+  </div>
+</div>";
+
+var data = new {
+    title = "My new post",
+    body = "This is my first post!"
+};
+
+string GetRenderedText(string tmpl)
+{
+    string result = string.Empty;
+    if (!string.IsNullOrWhiteSpace(tmpl))
+    {
+        var compiledTmpl = Handlebars.Compile(tmpl);
+        
+        result = compiledTmpl(data);
+    }
+
+    return result;
+}
+
+Console.WriteLine(GetRenderedText(source));
