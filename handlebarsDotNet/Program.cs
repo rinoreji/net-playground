3c60d570c5ae545b1d20dc2df21513312a439ff7
Rino Reji Cheriyan <mailrino@gmail.com>
2023-02-16 03:58:03 +0000

    Case insensitive key comparison

--- handlebarsDotNet/Program.cs
+++ handlebarsDotNet/Program.cs
@@ -1,22 +1,39 @@
 ﻿using HandlebarsDotNet;
 
+
+
 Handlebars.RegisterHelper("sph", (writer, context, arguments) =>
 {
   writer.Write($"{arguments[0]} {arguments[1]}");
 });
+Handlebars.RegisterHelper("ph", (writer, context, arguments) =>
+            {
+                if (arguments.Length < 1)
+                    throw new InvalidOperationException("Atleast one argument is mandatory while using '#ph' helper");
+                var phKey = arguments[0] as string;
+
+                var dataDict = new Dictionary<string, object>(context.Value as Dictionary<string, object>, StringComparer.OrdinalIgnoreCase);
+                //OrdinalIgnoreCase is the fastest as it dont have to look for culture specific rules while comparing
+                if (dataDict.ContainsKey(phKey) && !string.IsNullOrWhiteSpace(dataDict[phKey]?.ToString()))
+                    writer.Write(dataDict[phKey]);
+                else
+                {
+                    writer.WriteSafeString($"<span style='color:red'><eos-ph>{(arguments.Length > 1 ? arguments[1] : arguments[0])}</eos-ph></span>");
+                }
+            });
 
 string source =
 @"<div class=""entry"">
-  <h1>{{title}} {{#sph 'by' 'Rino'}}</h1>
+  <h1>{{Title}} {{#sph 'by' 'Rino'}}</h1>
   <div class=""body"">
-    {{body}}
+    {{body}} {{ #Ph 'title' 'New Joiner
+    FirstName' }}
   </div>
 </div>";
 
-var data = new {
-    title = "Post",
-    body = "A sample body"
-};
+var data = new Dictionary<string, object>();
+data.Add("Title","Post");
+data.Add("body","Sample body");
 
 string GetRenderedText(string tmpl)
 {
